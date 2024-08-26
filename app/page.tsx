2af2fc6483fe69d31035ca3eccf278efcd8a2032
reypjbaliguat <reypjbaliguat@gmail.com>
2024-08-26 22:42:09 +0800

    fix(homePage): unused file deleted

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useSession } from 'next-auth/react';
-
-export default function Home() {
-    const session = useSession();
-    return <main className='flex h-full w-full' />;
-}
