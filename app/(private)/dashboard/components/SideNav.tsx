4ecf223d1194fa660ec61987d38230003562ffe5
reypjbaliguat <reypjbaliguat@gmail.com>
2024-08-27 19:42:08 +0800

    feat(sideNav): added open/close menu

--- app/(private)/dashboard/components/SideNav.tsx
+++ app/(private)/dashboard/components/SideNav.tsx
@@ -1,15 +1,23 @@
+'use client';
+
+import { useAppSelector } from 'core/redux/hooks';
 import Link from 'next/link';
 import SideNavLinks from './SideNavLinks';
 
 function SideNav() {
+    const { isOpen } = useAppSelector((state) => state.menu);
     return (
-        <div className='flex w-[250px] max-w-[250px] flex-col border border-gray-200 p-5'>
+        <div
+            className={`flex flex-col border border-gray-200 py-5 transition-all duration-1000 ${isOpen ? 'w-[250px] max-w-[250px] px-5' : 'w-[80px] max-w-[80px] px-2'}`}
+        >
             <Link href={'/dashboard/games'}>
-                <h1 className='w-full rounded-md border bg-blue-500 py-2 text-center text-xl text-white transition-all duration-1000 hover:border-blue-500 hover:bg-gray-50 hover:text-blue-500'>
+                <h1
+                    className={`w-full rounded-md border bg-blue-500 py-2 text-center text-white transition-all duration-1000 hover:border-blue-500 hover:bg-gray-50 hover:text-blue-500 ${isOpen ? 'text-xl' : 'text-sm'}`}
+                >
                     Betting Site
                 </h1>
             </Link>
-            <SideNavLinks />
+            <SideNavLinks isOpen={isOpen} />
         </div>
     );
 }
--- app/(private)/dashboard/components/SideNavLinks.tsx
+++ app/(private)/dashboard/components/SideNavLinks.tsx
@@ -5,7 +5,7 @@ import Icon from '@mdi/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-function SideNavLinks() {
+function SideNavLinks({ isOpen }: { isOpen: boolean }) {
     const pathname = usePathname();
     return (
         <div className='mt-7 flex flex-col gap-y-2'>
@@ -13,10 +13,10 @@ function SideNavLinks() {
                 return (
                     <Link href={link.href} key={link.href}>
                         <div
-                            className={`flex w-full items-center justify-start rounded-md border border-gray-200 px-4 py-3 transition-all duration-1000 ${pathname.includes(link.href) && 'justify-center bg-blue-500 px-4 text-white'}`}
+                            className={`flex w-full items-center justify-start rounded-md border border-gray-200 py-3 transition-all duration-1000 ${pathname.includes(link.href) && 'justify-center bg-blue-500 text-white'} ${isOpen ? 'px-4' : 'px-1'}`}
                         >
                             <Icon path={link.icon} size={1} />
-                            <span className='ml-2'>{link.label}</span>
+                            {isOpen && <span className='ml-2'>{link.label}</span>}
                         </div>
                     </Link>
                 );
